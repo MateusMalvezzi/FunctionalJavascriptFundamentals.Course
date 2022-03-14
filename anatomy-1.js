f74c1a24ca88ec70660a863b4bfa85e3fa445d54
Mateus Malvezzi <mateusmalvezzir@hotmail.com>
2022-03-14 19:54:17 -0300

    notes about first class

--- anatomy-1.js
+++ anatomy-1.js
@@ -1,1 +1,36 @@
 // Function declaration
+
+// 01 Função que não recebe parâmetro e não retorna nada.
+function sayHello() {
+    console.log('Hello')
+}
+sayHello()
+
+// 02 Função que não retorna nada mas recebe um parâmetro.
+function sayHelloTo(name){
+    console.log('Hello ' + name)
+}
+sayHelloTo('Mike')
+// Ou podemos utilizar o template string
+function sayHelloTo(name){
+    console.log(`Hello ${name}!`)
+}
+sayHelloTo('Mike')
+
+// 03 Função que não recebe parâmetro e retorna valor.
+function returnHi(){
+    return 'Hi!'
+}
+returnHi()
+// Aqui, ela não retornará nada pois a função não imprimiu nada no console. Para retornar, faremos:
+function returnHi(){
+    return 'Hi!'
+}
+let greeting = returnHi()
+console.log(greeting) // Mesma coisa de utilizar console.log(returnHi())
+
+// 04 Função que recebe parâmetro e retorna valor.
+function returnHiTo(name) {
+    return `Hi ${name}!`
+}
+console.log(returnHiTo('John'))
