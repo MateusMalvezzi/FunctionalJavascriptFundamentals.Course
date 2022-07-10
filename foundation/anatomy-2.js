867605bbe65580cc9b052f355061000bc469dc07
Mateus Malvezzi <mmalvezzi@limaconsulting.com>
2022-07-09 22:32:46 -0300

    Learning about function in JS

--- foundation/anatomy-2.js
+++ foundation/anatomy-2.js
@@ -4,3 +4,17 @@ const sum = function (a, b){
 }
 const result = sum(10, 65)
 console.log(result)
+
+const anotherSum = sum
+console.log(anotherSum(9, 8))
+
+let x = 3
+// 3() doesn't work because it's not a function
+// x()doesn't work because it's not a function
+console.log(x)
+
+x = sum
+console.log(x(18, 9))
+
+// É possível atribuir ao valor de uma variável ou mesmo de uma constante o valor de uma função.
+// It is possible to assign to the value of a variable or even a constant the value of a function.
